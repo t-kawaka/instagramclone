841999194b500179d20a9b51f257215cbacde857
Takahiro Kawakami <t.kawaka1136@gmail.com>
2019-03-05 04:59:56 +0000

    favoriteテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190303055858) do
+ActiveRecord::Schema.define(version: 20190305045907) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,23 @@ ActiveRecord::Schema.define(version: 20190303055858) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_blogs_on_user_id"
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "blog_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.text "introduce"
+    t.text "picture"
+    t.integer "number"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,4 +59,5 @@ ActiveRecord::Schema.define(version: 20190303055858) do
   end
 
   add_foreign_key "blogs", "users"
+  add_foreign_key "profiles", "users"
 end
