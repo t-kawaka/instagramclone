ff3296a926c33fcc49f4ec771eb9d55865de9e3c
Takahiro Kawakami <t.kawaka1136@gmail.com>
2019-03-05 15:59:10 +0000

    favoriteモデルとのアソシエーション作成（blog.rb）

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,7 @@
 class Blog < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_users, through: :favorites, source: :user
   belongs_to :user
   mount_uploader :image, ImageUploader
   validates :content, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :username, presence: true
-   has_many :blogs, dependent: :destroy
-   has_many :favorites, dependent: :destroy
-   mount_uploader :profile, ProfileUploader
+  has_many :blogs, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  mount_uploader :profile, ProfileUploader
 end
