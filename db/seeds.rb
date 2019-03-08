a1b2d10bf9f850159b6d7070334beff01539da34
Takahiro Kawakami <t.kawaka1136@gmail.com>
2019-03-08 05:35:01 +0000

    seed作成中

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,23 @@
 
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 
 5.times do |n|
-  name = Faker::Dragonball.name
-  username = Faker::Dragonball.username
-  email = Faker::Internet.email
-  password = "foobar"
-  profile = "/stamp/public/うまる.jpg"
-  
-  user_id = Faker:: Number.between(1,5)
-  content = Faker::Internet.content
-  image = "/stamp/public/メインビジュアル.jpg"
+  name  = "name #{n+1}"
+  username = "username #{n+1}"
+  email = "example-#{n+1}@railstutorial.org"
+  password  = "foobar"
+  profile = "/stamp/public/umaru.jpg"
   
   User.create!( name: name,
+                username: username,
                 email: email,
                 profile: profile,
                 password: password,
                 password_confirmation: password,
                 )
+                
+  user_id = Faker::Number.between(1,5)
+  content = "content #{n+1}"
+  image = "/stamp/public/castle.jpg"
   Blog.create!( content: content,
                 image: image,
                 user_id: user_id,
