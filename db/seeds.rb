e51f3e199f3855dccc4ed2a4e75f1b2e4e2eecd0
Takahiro Kawakami <t.kawaka1136@gmail.com>
2019-03-09 16:41:01 +0000

    seed作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,24 @@
-
-
-5.times do |n|
+2.times do |n|
   name  = "name #{n+1}"
   username = "username #{n+1}"
-  email = "example-#{n+1}@railstutorial.org"
+  email = "test#{n+1}@gmail.com"
   password  = "foobar"
-  profile = "/stamp/public/umaru.jpg"
   
   User.create!( name: name,
                 username: username,
                 email: email,
-                profile: profile,
+                profile: File.open("./app/assets/images/umaru.jpg"),
                 password: password,
                 password_confirmation: password,
                 )
                 
-  user_id = Faker::Number.between(1,5)
+end
+
+2.times do |n|
+  user_id = Faker::Number.between(13,14)
   content = "content #{n+1}"
-  image = "/stamp/public/castle.jpg"
   Blog.create!( content: content,
-                image: image,
+                image: File.open("./app/assets/images/castle.jpg"),
                 user_id: user_id,
                 )
 end
