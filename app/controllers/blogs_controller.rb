118da60af2ffcc621ad8eecdd7587ae5286fb118
Takahiro Kawakami <t.kawaka1136@gmail.com>
2019-03-09 18:03:24 +0000

    kaminari導入

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,7 +2,7 @@ class BlogsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_blog, only: [:show, :edit, :update, :destroy]
   def index
-    @blogs = Blog.all
+    @blogs = Blog.page(params[:page])
   end
   
   def new
