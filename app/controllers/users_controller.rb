13fa75d2324d2b8f2d0b52ae28d46632929a9c44
Takahiro Kawakami <t.kawaka1136@gmail.com>
2019-03-12 05:02:02 +0000

    インデント修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-   before_action :set_blog, only: [:edit, :update]
+  before_action :set_blog, only: [:edit, :update]
   
   def edit
     @user = User.find(params[:id])
