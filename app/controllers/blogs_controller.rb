6dcc29abbbb4a0feee37d8b6288b57d8a1c4131a
Takahiro Kawakami <t.kawaka1136@gmail.com>
2019-03-16 02:24:27 +0000

    confirmの際にindexに移動

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -17,7 +17,7 @@ class BlogsController < ApplicationController
     @blog = current_user.blogs.new(blog_params)
     if @blog.save
        BlogMailer.blog_mail(@blog).deliver
-       redirect_to new_blog_path, notice:"ブログを作成しました"
+       redirect_to blogs_path, notice:"ブログを作成しました"
     else
       render 'new'
     end
