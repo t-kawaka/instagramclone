7d60b0843f7dcd8be51962fab6945cc59f73104c
Takahiro Kawakami <t.kawaka1136@gmail.com>
2019-03-17 06:55:15 +0000

    sign_upの画面で電話番号に関するエラーが表示しないよう設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :username, presence: true
-  validates :Telnumber, numericality: true, length: { maximum: 10 }
+  validates :Telnumber, numericality: true, length: { in: 9..10 }, on: :update
   has_many :blogs, dependent: :destroy
   has_many :favorites, dependent: :destroy
   mount_uploader :profile, ProfileUploader
