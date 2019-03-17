e470fe9aa31535f05bacc9348d1c5d8c9cdc56a2
Takahiro Kawakami <t.kawaka1136@gmail.com>
2019-03-17 06:29:07 +0000

    cookieをクリックするとinstagramのページに移動する

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :username, presence: true
-  validates :Telnumber, length: { maximum: 10 }, numericality: { only_integer: true}
+  validates :Telnumber, numericality: true, length: { maximum: 10 }
   has_many :blogs, dependent: :destroy
   has_many :favorites, dependent: :destroy
   mount_uploader :profile, ProfileUploader
