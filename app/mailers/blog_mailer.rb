22ce59a3d0a70e68aabacdcf60239cc63a543865
Takahiro Kawakami <t.kawaka1136@gmail.com>
2019-03-17 12:34:20 +0000

    mailer修正

--- app/mailers/blog_mailer.rb
+++ app/mailers/blog_mailer.rb
@@ -2,6 +2,6 @@ class BlogMailer < ApplicationMailer
   def blog_mail(blog)
     @blog = blog
 
-  mail to: "@user.email", subject: "お問い合わせの確認メール"
+  mail(to: @blog.user.email, subject: '私の素敵なサイトへようこそ')
   end
 end
