11e825325d8c30ad5a99af77b35ce93ca55f0199
Takahiro Kawakami <t.kawaka1136@gmail.com>
2019-03-18 08:36:11 +0000

    各ページのインデント修正

--- app/mailers/blog_mailer.rb
+++ app/mailers/blog_mailer.rb
@@ -1,7 +1,6 @@
 class BlogMailer < ApplicationMailer
   def blog_mail(blog)
     @blog = blog
-
-  mail(to: @blog.user.email, subject: '投稿のお知らせ')
+    mail(to: @blog.user.email, subject: '投稿のお知らせ')
   end
 end
